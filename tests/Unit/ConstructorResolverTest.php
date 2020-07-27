a9f484db5ffa25eac82f32965dd16165801dfa3c
peter279k <peter279k@gmail.com>
2020-07-27 18:42:42 +0800

    Add ConstructorResolver class test

---
+++ tests/Unit/ConstructorResolverTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Selective\Tests\Container\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Selective\Container\Container;
+use Selective\Container\Exceptions\InvalidDefinitionException;
+use Selective\Container\Resolver\ConstructorResolver;
+
+class ConstructorResolverTest extends TestCase
+{
+    /**
+     * Test.
+     *
+     * @return void
+     */
+    public function testResolveOnInvalidDefinition(): void
+    {
+        $this->expectException(InvalidDefinitionException::class);
+
+        $container = new Container();
+        $invalidId = 'invalid_id';
+
+        $constructorResolver = new ConstructorResolver($container);
+        $constructorResolver->resolve($invalidId);
+    }
+}
